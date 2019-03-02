c2d82727014f12b825cd23efa42be53f8945a563
Fernando Jerez <fernandojerez@gmail.com>
2019-03-02 16:53:28 -0500

    update jacoco version to 0.8.3

--- src/main/java/ff/camaro/plugin/gradle_plugin/Java.java
+++ src/main/java/ff/camaro/plugin/gradle_plugin/Java.java
@@ -44,7 +44,7 @@ public class Java implements GradlePlugin {
 		project.getPluginManager().apply(JacocoPlugin.class);
 
 		final JacocoPluginExtension extension = project.getExtensions().getByType(JacocoPluginExtension.class);
-		extension.setToolVersion("0.8.2");
+		extension.setToolVersion("0.8.3");
 
 		final JacocoReport report = (JacocoReport) project.getTasks().getByName("jacocoTestReport");
 		final JacocoReportsContainer reports = report.getReports();
