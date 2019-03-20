d1675175eef1ec3f94bc74b3830925065c67ba8d
Fernando Jerez <fernandojerez@gmail.com>
2019-03-20 07:35:01 -0500

    Add clean other folders in test cases to ensure that code is recompiled

--- src/main/java/ff/camaro/plugin/tasks/FFCleanTest.java
+++ src/main/java/ff/camaro/plugin/tasks/FFCleanTest.java
@@ -1,6 +1,7 @@
 package ff.camaro.plugin.tasks;
 
 import java.io.File;
+import java.io.FileFilter;
 
 import org.gradle.api.tasks.TaskAction;
 
@@ -14,6 +15,45 @@ public class FFCleanTest extends CamaroTask {
 		getProject().delete(new File(getProject().getBuildDir(), "reports/tests"));
 		getProject().delete(new File(getProject().getBuildDir(), "reports/jacoco"));
 		getProject().delete(new File(getProject().getBuildDir(), "jacoco"));
+
+		File dir = new File(getProject().getBuildDir(), "analized_test");
+		if (dir.exists()) {
+			for (final File folder : dir.listFiles(new FileFilter() {
+
+				@Override
+				public boolean accept(final File pathname) {
+					return pathname.exists();
+				}
+			})) {
+				getProject().delete(folder);
+			}
+		}
+
+		dir = new File(getProject().getBuildDir(), "macros_test");
+		if (dir.exists()) {
+			for (final File folder : dir.listFiles(new FileFilter() {
+
+				@Override
+				public boolean accept(final File pathname) {
+					return pathname.exists();
+				}
+			})) {
+				getProject().delete(folder);
+			}
+		}
+
+		dir = new File(getProject().getBuildDir(), "classes");
+		if (dir.exists()) {
+			for (final File folder : dir.listFiles(new FileFilter() {
+
+				@Override
+				public boolean accept(final File pathname) {
+					return pathname.exists() && pathname.getName().startsWith("ff_");
+				}
+			})) {
+				getProject().delete(new File(folder, "test"));
+			}
+		}
 	}
 
 }
