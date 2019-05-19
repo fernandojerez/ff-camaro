df583e806b22da30d9b9ed5f694e337f479c8cdb
Fernando Jerez <fernandojerez@gmail.com>
2019-05-19 17:35:02 -0500

    add charger to macro classloader to support new compiler

--- src/main/java/ff/camaro/plugin/tasks/FFCompiler.java
+++ src/main/java/ff/camaro/plugin/tasks/FFCompiler.java
@@ -92,6 +92,16 @@ public class FFCompiler extends CamaroTask {
 		}
 
 		try {
+			if (getProject().getName().equals("ff@charger")) {
+				String getenv = System.getenv("FF_REPO");
+				if (getenv.startsWith("file://")) {
+					getenv = getenv.substring("file://".length());
+				}
+				final File charger = new File(getenv + "/ff/charger/0.0.1/charger-0.0.1-java.jar");
+				if (charger.exists()) {
+					macroFiles.add(charger.toURI().toURL());
+				}
+			}
 			final Configuration conf = getProject().getConfigurations().getByName("macros");
 			for (final File file : conf.getFiles()) {
 				macroFiles.add(file.toURI().toURL());
