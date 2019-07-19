0dec9451db0d62e865c98f9a2de7d35156a685f0
Fernando Jerez <fernandojerez@gmail.com>
2019-07-18 21:41:51 -0500

    add ff-java facet

---
+++ src/main/java/ff/camaro/plugin/gradle_plugin/Ivy.java
@@ -0,0 +1,15 @@
+package ff.camaro.plugin.gradle_plugin;
+
+import org.gradle.api.Project;
+import org.gradle.api.publish.ivy.plugins.IvyPublishPlugin;
+
+import ff.camaro.Configurator;
+
+public class Ivy implements GradlePlugin {
+
+	@Override
+	public void apply(final Project target, final Configurator configurator) {
+		target.getPluginManager().apply(IvyPublishPlugin.class);
+	}
+
+}
